01a830c3ddca7f7e2f6329c7d9d8a2546062fe67
SpencerTower <spencerjtower1@gmail.com>
2021-12-09 19:19:53 -0800

    adds card to Movies.js

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card } from 'react-bootstrap/Card';
 
 export default class Movies extends Component {
 
   render() {
     return (
       <div>
-        <h3 style={{ color: 'blue' }}>{this.props.movie.title}</h3>
-
-        <ul>
-          {this.props.movieData.map((movie, idx) => <li key={movie.idx}>{movie.title}</li>)}
-        </ul>
+        <Card style={{ width: '20rem' }}>
+          <Card.Img variant="top" src={this.props.movie.img_url} alt={this.props.movie.title} title={this.props.movie.title} />
+          <Card.Body>
+            <Card.Title>{this.props.movie.title}</Card.Title>
+            <Card.Text>
+              <p>{this.props.movie.description}</p>
+              <p>Release Date:{this.props.movie.release_date}</p>
+              <p>Average Rating:{this.props.movie.popularity}</p>
+              <p>Total Votes:{this.props.movie.total_votes}</p>
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </div>
     )
   }
 }
+
+
 
 // "title": "Sleepless in Seattle",
 // "overview": "A young boy who tries to set his dad up on a date after the death of his mother. He calls into a radio station to talk about his dad’s loneliness which soon leads the dad into meeting a Journalist Annie who flies to Seattle to write a story about the boy and his dad. Yet Annie ends up with more than just a story in this popular romantic comedy.",
