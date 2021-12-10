35f122c98140070cc46ea5accfd699e5dbae4b42
SpencerTower <spencerjtower1@gmail.com>
2021-12-09 22:00:19 -0800

    cleans up App.js and Movies.js

--- src/App.js
+++ src/App.js
@@ -46,14 +46,25 @@ export default class App extends Component {
   }
 
   getWeatherData = async (city_name) => {
-    const weatherDataRes = await axios.get(`${process.env.REACT_APP_URL}/weather?city_name=${city_name}&lat=${this.state.locationData.lat}&lon=${this.state.locationData.lon}`);
-    this.setState({ weatherData: weatherDataRes.data });
+    try {
+      const weatherDataRes = await axios.get(`${process.env.REACT_APP_URL}/weather?city_name=${city_name}&lat=${this.state.locationData.lat}&lon=${this.state.locationData.lon}`);
+      this.setState({ weatherData: weatherDataRes.data });
+      this.setState({ error: false });
+    } catch (error) {
+      this.setState({ error: true });
+
+    }
   }
 
   getMovieData = async (city_name) => {
-    const movieRes = await axios.get(`${process.env.REACT_APP_URL}/movies?city_name=${city_name}`);
-    this.setState({ movieData: movieRes.data });
-    console.log(this.state.movieData);
+    try {
+      const movieRes = await axios.get(`${process.env.REACT_APP_URL}/movies?city_name=${city_name}`);
+      this.setState({ movieData: movieRes.data });
+      console.log(this.state.movieData);
+      this.setState({ error: false });
+    } catch (error) {
+      this.setState({ error: true })
+    }
   }
   // getMovieData = async () => {
   //   const movieData = await axios.get()
@@ -77,13 +88,11 @@ export default class App extends Component {
         {this.state.error && <Error city={this.state.city} />}
         {/* if data in weatherData, send props into  */}
         {this.state.weatherData.length > 0 && <Weather weatherData={this.state.weatherData} error={this.state.error} />}
-        {/* map movies into Movies component and render */}
+        {/* map movies into Movies component to render */}
         <Row sm={1} md={2} lg={5}>
           {console.log('Movie Data:', this.state.movieData)}
           {this.state.movieData.length > 0 && this.state.movieData.map(movie => <Movies movie={movie} />)}
         </Row>
-        {/* this.state.movies.map(movie =>  */}
-
       </div >
     )
   }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,17 +6,15 @@ export default class Movies extends Component {
   render() {
     return (
       <>
-        {/* <h3>{this.props.movie.title}</h3>
-        <img src={this.props.movie.img_url} alt={this.props.movie.title} /> */}
         <Card style={{ width: '20rem' }}>
           <Card.Img variant="top" src={this.props.movie.img_url} alt={this.props.movie.title} title={this.props.movie.title} />
           <Card.Body>
             <Card.Title>{this.props.movie.title}</Card.Title>
             <Card.Text>
               <p>{this.props.movie.overview}</p>
-              <p>Release Date:{this.props.movie.release_date}</p>
-              <p>Average Rating:{this.props.movie.popularity}</p>
-              <p>Total Votes:{this.props.movie.total_votes}</p>
+              <p>Release Date: {this.props.movie.release_date}</p>
+              <p>Average Rating: {this.props.movie.popularity}</p>
+              <p>Total Votes: {this.props.movie.total_votes}</p>
             </Card.Text>
           </Card.Body>
         </Card>
@@ -24,21 +22,3 @@ export default class Movies extends Component {
     )
   }
 }
-
-
-
-// "title": "Sleepless in Seattle",
-// "overview": "A young boy who tries to set his dad up on a date after the death of his mother. He calls into a radio station to talk about his dad’s loneliness which soon leads the dad into meeting a Journalist Annie who flies to Seattle to write a story about the boy and his dad. Yet Annie ends up with more than just a story in this popular romantic comedy.",
-// "average_votes": "6.60",
-// "total_votes": "881",
-// "image_url": "https://image.tmdb.org/t/p/w500/afkYP15OeUOD0tFEmj6VvejuOcz.jpg",
-// "popularity": "8.2340",
-// "released_on": "1993-06-24"
-
-// this.title = obj.title;
-// this.overview = obj.overview;
-// this.avg_votes = obj.average_votes;
-// this.total_votes = obj.total_votes;
-// this.img_url = obj.image_url;
-// this.popularity = obj.popularity;
-// this.release_date = obj.released_on;
